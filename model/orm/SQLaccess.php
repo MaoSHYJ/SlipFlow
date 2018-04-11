9dedbfc583d3a31a33a6967771cc91635f41d7e1
MaoSYJ <maoshyj@gmail.com>
2018-04-12 01:19:51 +0200

    refactor of DatabaseAccess the conection to the database. Cleaned code of the private proyecto

--- model/orm/SQLaccess.php
+++ model/orm/SQLaccess.php
@@ -1,33 +1,36 @@
 <?php
 class DatabaseAccess{
- 
-  function conex()
-  {
-    try{
-      $user = "root";
-      $password = "root";//QUE NO SE TE OLVIDE CAMBIARLO A LA HORA DE DESPLEGAR
-      $base= 'mysql:host=localhost;dbname=PERIODICO';
-      $baseDatos = new PDO($base,$user,$password);
-      $baseDatos ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      return $baseDatos;
-    } catch (PDOException $e){
-      print "ERROR".$e->getMessage()."<br>";
-    }
-  }
-  
-  static function getUltimosComentarios(){
-    $baseDatos = DatabaseAccess::conex();
-    $comentarios = $baseDatos->prepare("SELECT ID_comentario, LEFT(com_contenido,300) as com_contenido, com_autor, art_referencia FROM comentario ORDER BY com_fecha DESC LIMIT 0, 20;");
-    $comentarios -> execute();
-    $i= 0;
-    while($resultado=$comentarios -> fetch(PDO::FETCH_ASSOC)) {
-      $contenido[$i]['ID_comentario'] = $resultado['ID_comentario'];
-      $contenido[$i]['comentario'] = $resultado['com_contenido'];
-      $contenido[$i]['autor'] = $resultado['com_autor'];
-      $contenido[$i]['referencia'] = $resultado['art_referencia'];
-      $i++;
-    }
-    return $contenido;
-  }
-}
+  private $database_data =[
+    'hostname'=> 'localhost',
+    'port'=>'666',
+    'userName'=>'fool',
+    'password'=>'edit',
+    'dataBaseName'=>'yourData'
+  ];
 
+  function __construct($dataBaseName){
+    $temp_data = $this->database_data['dataBaseName'];
+    $this->database_data['dataBaseName'] = $dataBaseName;
+    try{
+      $this->conex();
+    }catch(Exception $e){
+      $this->database_data['dataBaseName'] = $temp_data;
+    }
+  }
+
+  private function conex(){
+    try{
+      $user = $this->database_data['userName'];
+      $password = $this->database_data['password'];
+      $base = 'mysql:host=localhost;dbname='.$this->database_data['dataBaseName'];
+      $dataBase = new PDO($base,$user,$password);
+      $dataBase ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $dataBase;
+    } catch (PDOException $e){
+      throw new Exception($e);//
+    }
+  }
+
+
+  
+}
